7235adad738230f703fd61815b7083da457b5c4e
Sahil Khadka <sahilkhadka767@gmail.com>
2023-05-07 22:20:55 +0545

    image styling changes

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -8,9 +8,9 @@ type ImageCardProps = {
 function ImageCard(props: ImageCardProps) {
 	return (
 		<div className='container'>
-			<div className='w-80 h-80 my-8 mx-auto relative card_box'>
+			<div className='w-80 my-8 mx-auto relative card_box'>
 				<img
-					className='w-full object-cover h-full aspect-auto'
+					className='w-full object-contain  aspect-auto'
 					src={props.imageFile && URL.createObjectURL(props.imageFile)}
 					alt=''
 				/>
